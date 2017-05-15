1a0935154b28ba2aaa0f40897d262ebbdf63e303
Gasper Petelin <Gasper Petelin>
2017-05-15 16:28:11 +0200

    null test change

--- jmetal/operator/test/nullTest.py
+++ jmetal/operator/test/nullTest.py
@@ -18,15 +18,24 @@ class TestMethods(unittest.TestCase):
         operator = Null()
         self.assertEqual(0, operator.probability)
 
-    def test_should_the_solution_remain_unchanged(self):
+    def test_should_the_solution_remain_unchanged_float(self):
         operator = Null()
         solution = FloatSolution(number_of_variables=3, number_of_objectives=1)
         solution.variables = [1.0, 2.0, 3.0]
-        FloatSolution.lower_bound = [-5, -5, -5]
-        FloatSolution.upper_bound = [5, 5, 5]
+        solution.lower_bound = [-5, -5, -5]
+        solution.upper_bound = [5, 5, 5]
 
         mutated_solution = operator.execute(solution)
         self.assertEqual([1.0, 2.0, 3.0], mutated_solution.variables)
+
+    def test_should_the_solution_remain_unchanged_binary(self):
+        operator = Null()
+        solution = BinarySolution(number_of_variables=1, number_of_objectives=1)
+        solution.variables[0] = [True, True, False, False, True, False]
+
+        mutated_solution = operator.execute(solution)
+        self.assertEqual([True, True, False, False, True, False], mutated_solution.variables[0])
+
 
 if __name__ == '__main__':
     unittest.main()
