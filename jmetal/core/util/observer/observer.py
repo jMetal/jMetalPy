9db61d00d26ca8ad13321dadecfe6aa43828fd30
Antonio J. Nebro <antonio@lcc.uma.es>
2017-05-29 17:30:58 +0200

    Add unit tests in class observerTest.py

--- jmetal/core/util/observer/observer.py
+++ jmetal/core/util/observer/observer.py
@@ -1,9 +1,14 @@
 from abc import ABCMeta, abstractmethod
 
-
 class Observer(object):
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def update(self, *args, **kwargs):
+        '''
+        Update method
+        :param args:
+        :param kwargs:
+        :return:
+        '''
         pass
--- jmetal/core/util/observer/test/observerTest.py
+++ jmetal/core/util/observer/test/observerTest.py
@@ -3,17 +3,6 @@ import unittest
 from jmetal.core.util.observer.impl.defaultobservable import DefaultObservable
 from jmetal.core.util.observer.observer import Observer
 
-"""
-class MockObserver(Observer):
-    def __init__(self):
-        self.args = []
-        self.kwargs = {}
-
-    def update(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-        return
-"""
 
 class TestMethods(unittest.TestCase):
     def setUp(self):
@@ -24,6 +13,35 @@ class TestMethods(unittest.TestCase):
         self.observable.register(observer)
 
         self.assertEqual(1, len(self.observable.observers))
+
+    def test_should_register_add_two_observer(self):
+        self.observable.register(Observer())
+        self.observable.register(Observer())
+
+        self.assertEqual(2, len(self.observable.observers))
+
+    def test_should_register_add_two_observer(self):
+        self.observable.register(Observer())
+        self.observable.register(Observer())
+
+        self.assertEqual(2, len(self.observable.observers))
+
+    def test_should_unregister_remove_the_observer_if_it_is_registered(self):
+        observer = Observer()
+        self.observable.register(observer)
+        self.observable.unregister(observer)
+
+        self.assertEqual(0, len(self.observable.observers))
+
+    def test_should_unregister_not_remove_the_observer_if_it_is_not_registered(self):
+        observer = Observer()
+        observer2 = Observer()
+        self.observable.register(observer)
+        self.observable.unregister(observer2)
+
+        self.assertEqual(1, len(self.observable.observers))
+        self.assertTrue(observer in self.observable.observers)
+        self.assertFalse(observer2 in self.observable.observers)
 
 if __name__ == "__main__":
     unittest.main()
