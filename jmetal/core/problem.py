657b96e477bc1b9f4c11528f48314d0ffd220d9a
canicio <canicio7@gmail.com>
2017-05-31 14:15:18 +0200

    Refactor in modules (.py files)

---
+++ jmetal/core/problem.py
@@ -0,0 +1,42 @@
+from typing import Generic, TypeVar
+from jmetal.core.solution.binarySolution import BinarySolution
+from jmetal.core.solution.floatSolution import FloatSolution
+
+__author__ = "Antonio J. Nebro"
+
+S = TypeVar('S')
+
+
+class BinaryProblem(BinarySolution):
+    """ Class representing float problems """
+
+    def evaluate(self, solution: BinarySolution) -> None:
+        pass
+
+    def create_solution(self) -> BinarySolution:
+        pass
+
+
+class FloatProblem(FloatSolution):
+    """ Class representing float problems """
+
+    def evaluate(self, solution: FloatSolution) -> None:
+        pass
+
+    def create_solution(self) -> FloatSolution:
+        pass
+
+
+class Problem(Generic[S]):
+    """ Class representing problems """
+
+    def __init__(self):
+        self.number_of_variables = 0
+        self.number_of_objectives = 0
+        self.number_of_constraints = 0
+
+    def evaluate(self, solution: S) -> None:
+        pass
+
+    def create_solution(self) -> S:
+        pass
