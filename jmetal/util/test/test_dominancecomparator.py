7cde1488cf41fc75d6e603ed0f9fbb2203f6329d
ajnebro <antonio@lcc.uma.es>
2017-06-09 07:43:48 +0200

    Revised tests in jmetal.util

--- jmetal/util/test/test_dominancecomparator.py
+++ jmetal/util/test/test_dominancecomparator.py
@@ -3,8 +3,6 @@ import unittest
 from jmetal.core.solution import FloatSolution
 from jmetal.util.comparator import dominance_comparator
 
-__author__ = "Antonio J. Nebro"
-
 
 class TestMethods(unittest.TestCase):
 
@@ -13,25 +11,25 @@ class TestMethods(unittest.TestCase):
 
     def test_should_dominance_comparator_raise_an_exception_if_the_first_solution_is_null(self):
         solution = None
-        solution2 = FloatSolution(3, 2)
+        solution2 = FloatSolution(3, 2, 0, [], [])
         with self.assertRaises(Exception):
             dominance_comparator(solution, solution2)
 
     def test_should_dominance_comparator_raise_an_exception_if_the_second_solution_is_null(self):
-        solution = FloatSolution(3, 2)
+        solution = FloatSolution(3, 2, 0, [], [])
         solution2 = None
         with self.assertRaises(Exception):
             dominance_comparator(solution, solution2)
 
     def test_should_dominance_comparator_raise_an_exception_if_the_solutions_have_not_the_same_number_of_objectives(self):
-        solution = FloatSolution(3, 2)
-        solution2 = FloatSolution(3, 5)
+        solution = FloatSolution(3, 2, 0, [], [])
+        solution2 = FloatSolution(3, 5, 0, [], [])
         with self.assertRaises(Exception):
             dominance_comparator(solution, solution2)
 
     def test_should_dominance_comparator_return_zero_if_the_two_solutions_have_one_objective_with_the_same_value(self):
-        solution = FloatSolution(3, 1)
-        solution2 = FloatSolution(3, 1)
+        solution = FloatSolution(3, 1, 0, [], [])
+        solution2 = FloatSolution(3, 1, 0, [], [])
 
         solution.objectives = [1.0]
         solution2.objectives = [1.0]
@@ -39,8 +37,8 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(0, dominance_comparator(solution, solution2))
 
     def test_should_dominance_comparator_return_one_if_the_two_solutions_have_one_objective_and_the_second_one_is_lower(self):
-        solution = FloatSolution(3, 1)
-        solution2 = FloatSolution(3, 1)
+        solution = FloatSolution(3, 1, 0, [], [])
+        solution2 = FloatSolution(3, 1, 0, [], [])
 
         solution.objectives = [2.0]
         solution2.objectives = [1.0]
@@ -48,8 +46,8 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(1, dominance_comparator(solution, solution2))
 
     def test_should_dominance_comparator_return_minus_one_if_the_two_solutions_have_one_objective_and_the_first_one_is_lower(self):
-        solution = FloatSolution(3, 1)
-        solution2 = FloatSolution(3, 1)
+        solution = FloatSolution(3, 1, 0, [], [])
+        solution2 = FloatSolution(3, 1, 0, [], [])
 
         solution.objectives = [1.0]
         solution2.objectives = [2.0]
@@ -60,8 +58,8 @@ class TestMethods(unittest.TestCase):
         '''
         Case A: solution1 has objectives [-1.0, 5.0, 9.0] and solution2 has [2.0, 6.0, 15.0]
         '''
-        solution = FloatSolution(3, 3)
-        solution2 = FloatSolution(3, 3)
+        solution = FloatSolution(3, 3, 0, [], [])
+        solution2 = FloatSolution(3, 3, 0, [], [])
 
         solution.objectives = [-1.0, 5.0, 9.0]
         solution2.objectives = [2.0, 6.0, 15.0]
@@ -72,8 +70,8 @@ class TestMethods(unittest.TestCase):
         '''
         Case b: solution1 has objectives [-1.0, 5.0, 9.0] and solution2 has [-1.0, 5.0, 10.0]
         '''
-        solution = FloatSolution(3, 3)
-        solution2 = FloatSolution(3, 3)
+        solution = FloatSolution(3, 3, 0, [], [])
+        solution2 = FloatSolution(3, 3, 0, [], [])
 
         solution.objectives = [-1.0, 5.0, 9.0]
         solution2.objectives = [-1.0, 5.0, 10.0]
@@ -84,8 +82,8 @@ class TestMethods(unittest.TestCase):
         '''
         Case c: solution1 has objectives [-1.0, 5.0, 9.0] and solution2 has [-2.0, 5.0, 9.0]
         '''
-        solution = FloatSolution(3, 3)
-        solution2 = FloatSolution(3, 3)
+        solution = FloatSolution(3, 3, 0, [], [])
+        solution2 = FloatSolution(3, 3, 0, [], [])
 
         solution.objectives = [-1.0, 5.0, 9.0]
         solution2.objectives = [-2.0, 5.0, 9.0]
@@ -96,8 +94,8 @@ class TestMethods(unittest.TestCase):
         '''
         Case d: solution1 has objectives [-1.0, 5.0, 9.0] and solution2 has [-1.0, 5.0, 8.0]
         '''
-        solution = FloatSolution(3, 3)
-        solution2 = FloatSolution(3, 3)
+        solution = FloatSolution(3, 3, 0, [], [])
+        solution2 = FloatSolution(3, 3, 0 , [], [])
 
         solution.objectives = [-1.0, 5.0, 9.0]
         solution2.objectives = [-1.0, 5.0, 8.0]
@@ -108,8 +106,8 @@ class TestMethods(unittest.TestCase):
         '''
         Case d: solution1 has objectives [-1.0, 5.0, 9.0] and solution2 has [-2.0, 5.0, 10.0]
         '''
-        solution = FloatSolution(3, 3)
-        solution2 = FloatSolution(3, 3)
+        solution = FloatSolution(3, 3, 0, [], [])
+        solution2 = FloatSolution(3, 3, 0, [], [])
 
         solution.objectives = [-1.0, 5.0, 9.0]
         solution2.objectives = [-2.0, 5.0, 10.0]
