fefd3cb4b79592ff30ea9dad65d9774420034363
canicio <canicio7@gmail.com>
2017-06-12 17:21:35 +0200

    Setup parameters

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(name='jmetalpy',
+      version='1.0.0',
+      description='JMetalPy',
+      author='Antonio Nebro',
+      author_email='ajnebro@uma.es',
+      maintainer='Antonio Nebro, Carlos Canicio',
+      maintainer_email='ajnebro@uma.es, canicio@lcc.uma.es',
+      license='GNU GENERAL PUBLIC LICENSE',
+      packages=find_packages(),
+      )
