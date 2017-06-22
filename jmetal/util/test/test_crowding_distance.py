2901d45b84e651a65a126d525acb730ba74a561a
Álvaro Gómez Jáuregui <alvarogj@lcc.uma.es>
2017-06-22 09:50:17 +0200

    More tests over crowding algth

--- jmetal/util/test/test_crowding_distance.py
+++ jmetal/util/test/test_crowding_distance.py
@@ -20,3 +20,15 @@ class CrowdingDistanceTestCases(unittest.TestCase):
         value = solution_list[0].attributes["distance"]
 
         self.assertEqual(float("inf"), value)
+
+    def should_the_crowding_distance_of_two_solutions_be_infinity(self):
+        solution1 = Solution(3, 3)
+        solution2 = Solution(3, 3)
+        solution_list = [solution1, solution2]
+
+        self.crowding.compute_density_estimator(solution_list)
+        value_from_solution1 = solution_list[0].attributes["distance"]
+        value_from_solution2 = solution_list[1].attributes["distance"]
+
+        self.assertEqual(float("inf"), value_from_solution1)
+        self.assertEqual(float("inf"), value_from_solution2)
