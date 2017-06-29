1fc194c0df71c051196a224be200bef78055af05
dailymp <dailymp@gmail.com>
2017-06-29 02:14:05 +0200

    Adding test cases

---
+++ jmetal/core/test/test_algorithm.py
@@ -0,0 +1,41 @@
+from jmetal.algorithm.singleobjective.evolutionaryalgorithm import GenerationalGeneticAlgorithm
+from jmetal.core.solution import BinarySolution, FloatSolution
+from jmetal.operator.crossover import SinglePoint, SBX
+from jmetal.operator.mutation import BitFlip, Polynomial
+from jmetal.operator.selection import BinaryTournament
+from jmetal.problem.singleobjectiveproblem import OneMax, Sphere
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+import unittest
+import pytest
+
+class AlgoritmTestIntegrationTestCases(unittest.TestCase):
+
+    def setUp(self):
+        bits = 512
+        self.problem = OneMax(bits)
+        self.algorithm = GenerationalGeneticAlgorithm[BinarySolution, BinarySolution](
+            self.problem,
+            population_size=100,
+            max_evaluations=25000,
+            mutation=BitFlip(1.0 / bits),
+            crossover=SinglePoint(0.9),
+            selection=BinaryTournament())
+
+
+    def test_algorithm_genetic(self):
+        self.algorithm.run()
+        result = self.algorithm.get_result()
+        logger.info("Algorithm (binary problem): " + self.algorithm.get_name())
+        logger.info("Problem: " +  self.problem.get_name())
+        logger.info("Solution: " + str(result.variables[0]))
+        logger.info("Fitness:  " + str(result.objectives[0]))
+        logger.info("Computing time: " + str(self.algorithm.total_computing_time))
+
+        assert( -500 <= result.objectives[0] <= 500)
+
+
