1e6e1197cf3c938b8e12f05e0e0a99a296cb4fef
benhid <antonio.b@uma.es>
2017-07-07 13:17:33 +0200

    Added logger

--- jmetal/runner/singleobjective/genetic_algorithm/generational_genetic_algorithm_with_observer.py
+++ jmetal/runner/singleobjective/genetic_algorithm/generational_genetic_algorithm_with_observer.py
@@ -1,31 +1,17 @@
-#from graphtiny.bs import DataStreamWindowBS, ChartBS
-#from graphtiny.domain import DataStreamWindow, Chart
+import logging
 
 from jmetal.algorithm.singleobjective.evolutionaryalgorithm import GenerationalGeneticAlgorithm
-from jmetal.component.consumer import BasicSingleObjectiveAlgorithmConsumer
+from jmetal.component.observer import BasicSingleObjectiveAlgorithmObserver
 from jmetal.core.solution import FloatSolution
 from jmetal.operator.crossover import SBX
 from jmetal.operator.mutation import Polynomial
 from jmetal.operator.selection import BinaryTournament
 from jmetal.problem.singleobjectiveproblem import Sphere
-from jmetal.util.observable import Observer
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-"""
-class ChartAlgorithmObserver(Observer):
-    def __init__(self) -> None:
-        self.window = DataStreamWindow()
-        self.chart = Chart()
-        self.chart.left_label = 'Objectives'
-        self.chart.bottom_label = 'Evaluations'
-        self.window.charts_list.append(self.chart)
-        DataStreamWindowBS().launch_window(self.window)
-
-    def update(self, *args, **kwargs):
-        ChartBS().set_data_stream(self.chart, kwargs["computing time"], kwargs["best"].objectives[0])
-
-        ChartBS().set_data_stream(self.chart, kwargs["evaluations"], kwargs["best"].objectives[0])
-"""
 def main() -> None:
     variables = 10
     problem = Sphere(variables)
@@ -37,7 +23,7 @@ def main() -> None:
         crossover=SBX(1.0, distribution_index=20),
         selection=BinaryTournament())
 
-    observer = BasicSingleObjectiveAlgorithmConsumer()
+    observer = BasicSingleObjectiveAlgorithmObserver()
 
     algorithm.observable.register(observer=observer)
 
@@ -45,10 +31,10 @@ def main() -> None:
     algorithm.join()
 
     result = algorithm.get_result()
-    print("Algorithm: " + algorithm.get_name())
-    print("Problem: " + problem.get_name())
-    print("Solution: " + str(result.variables))
-    print("Fitness:  " + str(result.objectives[0]))
+    logger.info("Algorithm: " + algorithm.get_name())
+    logger.info("Problem: " + problem.get_name())
+    logger.info("Solution: " + str(result.variables))
+    logger.info("Fitness:  " + str(result.objectives[0]))
 
 
 if __name__ == '__main__':
