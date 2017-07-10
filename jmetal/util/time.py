bdae1d141f78a4fb7f74ef8e1c5dd91ec9225006
benhid <antonio.b@uma.es>
2017-07-10 23:42:12 +0200

    Fix typo

--- jmetal/util/time.py
+++ jmetal/util/time.py
@@ -5,13 +5,14 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def get_time_of_execution(f):
+def get_time_of_execution(fnc):
     """ Decorator to get time of execution of any method inside a class. """
 
     def wrapped(*args, **kwargs):
         start_time = time.time()
-        res = f(*args, **kwargs)
-        logger.info("Computing time to " + f.__name__ + " (in seconds): " + str(time.time() - start_time))
+        res = fnc(*args, **kwargs)
+        logger.info("Computing time to " + fnc.__name__ + " (in seconds): " + str(time.time() - start_time))
+
         return res
 
     return wrapped
