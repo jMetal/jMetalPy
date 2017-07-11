597db8d5df519cfa5b85f6b3f64cfb6854f23fd6
benhid <antonio.b@uma.es>
2017-07-11 14:32:29 +0200

    Simplified version of get_data_points

--- jmetal/util/solution_list_output.py
+++ jmetal/util/solution_list_output.py
@@ -10,42 +10,35 @@ logger = logging.getLogger(__name__)
 S = TypeVar('S')
 
 
-def get_data(solution_list: List[S]) -> Tuple[list, list]:
-    sol = []
-    for solution in solution_list:
-        for function_value in solution.objectives:
-            sol.append(function_value)
+def get_data_points(solution_list: List[S]) -> Tuple[list, list]:
+    points = list(sol.objectives for sol in solution_list)
+    x_values, y_values = [x[0] for x in points], [y[1] for y in points]
 
-    values = [tuple(sol[i:i + 2]) for i in range(0, len(sol), 2)]
-
-    x_val = [x[0] for x in values]
-    y_val = [x[1] for x in values]
-
-    return x_val, y_val
+    return x_values, y_values
 
 
 class SolutionListOutput(Generic[S]):
     @staticmethod
     def plot_scatter(file_name, solution_list:List[S]):
-        """ Plot non-dominated solutions.
+        """ Plot non-dominated solutions. For problems with TWO variables.
         """
-        x_val, y_val = get_data(solution_list)
+        x_values, y_values = get_data_points(solution_list)
 
         sc = ScatterPlot(plot_title="Pareto frontier")
-        sc.simple_plot(x_val, y_val, save=True, file_name=file_name)
+        sc.simple_plot(x_values, y_values, save=True, file_name=file_name)
 
     @staticmethod
-    def plot_scatter_real_time(solution_list:List[S], animation_speed: float = 1*10e-10):
-        """ Plot non-dominated solutions in real-time.
+    def plot_scatter_real_time(solution_list: List[S], animation_speed: float):
+        """ Plot non-dominated solutions in real-time. For problems with TWO variables.
         """
         global sc
-        x_val, y_val = get_data(solution_list)
+        x_values, y_values = get_data_points(solution_list)
 
         if not plt.get_fignums():
             # The first time, set up plot
             sc = ScatterPlot(plot_title="Pareto frontier", animation_speed=animation_speed)
         else:
-            sc.live_plot(x_val, y_val)
+            sc.live_plot(x_values, y_values, solution_list)
 
     @staticmethod
     def print_variables_to_screen(solution_list:List[S]):
