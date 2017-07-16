f6d2c06010d0809e24408bc64bcdf965e9ec7f01
Antonio J. Nebro <antonio@lcc.uma.es>
2017-07-16 11:21:38 +0200

    Minor changes

--- jmetal/runner/multiobjective/nsgaII_standard_settings_with_observer_plot_realtime.py
+++ jmetal/runner/multiobjective/nsgaII_standard_settings_with_observer_plot_realtime.py
@@ -17,7 +17,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-@get_time_of_execution
 def main() -> None:
     problem = ZDT1()
     algorithm = NSGAII[FloatSolution, List[FloatSolution]](
--- jmetal/runner/multiobjective/nsgaii_standard_settings.py
+++ jmetal/runner/multiobjective/nsgaii_standard_settings.py
@@ -16,7 +16,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-@get_time_of_execution
 def main() -> None:
     problem = ZDT1()
     algorithm = NSGAII[FloatSolution, List[FloatSolution]](
