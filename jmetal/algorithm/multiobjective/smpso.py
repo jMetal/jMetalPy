d78ae88f1a5f043ebb31d206e5e5827e22fee114
benhid <makishinake@hotmail.com>
2018-04-24 13:00:08 +0200

    Added SMPSO algorithm

--- jmetal/algorithm/multiobjective/smpso.py
+++ jmetal/algorithm/multiobjective/smpso.py
@@ -1,5 +1,6 @@
 from copy import copy
-from random import Random
+import random
+from math import sqrt
 from typing import TypeVar, List
 
 import numpy
@@ -29,7 +30,7 @@ class SMPSO(ParticleSwarmOptimization):
         self.problem = problem
         self.swarm_size = swarm_size
         self.max_evaluations = max_evaluations
-        self.mutation : Mutation[FloatSolution] = mutation
+        self.mutation: Mutation[FloatSolution] = mutation
         self.leaders = leaders
         self.observable = observable
         self.evaluator = evaluator
@@ -40,7 +41,10 @@ class SMPSO(ParticleSwarmOptimization):
         self.c1_max = 2.5
         self.c2_min = 1.5
         self.c2_max = 2.5
-
+        self.r1_min = 0.0
+        self.r1_max = 1.0
+        self.r2_min = 0.0
+        self.r2_max = 1.0
         self.min_weight = 0.1
         self.max_weight = 0.1
 
@@ -50,24 +54,25 @@ class SMPSO(ParticleSwarmOptimization):
         self.dominance_comparator = DominanceComparator()
 
         self.speed = numpy.zeros((self.swarm_size, self.problem.number_of_variables), dtype=float)
-        self.delta_max = numpy.empty(problem.number_of_variables)
-        self.delta_min = numpy.empty(problem.number_of_variables)
+        self.delta_max, self.delta_min = numpy.empty(problem.number_of_variables),\
+                                         numpy.empty(problem.number_of_variables)
         for i in range(problem.number_of_variables):
             self.delta_max[i] = (self.problem.upper_bound[i] - self.problem.lower_bound[i]) / 2.0
 
         self.delta_min = -1.0 * self.delta_max
 
-    def init_progress(self) -> None :
+    def init_progress(self) -> None:
         self.evaluations = self.swarm_size
         self.leaders.compute_density_estimator()
 
-    def update_progress(self) -> None :
+    def update_progress(self) -> None:
         self.evaluations += self.swarm_size
         self.leaders.compute_density_estimator()
 
         observable_data = {'evaluations': self.evaluations,
-                           'population': self.swarm,
+                           'population': self.leaders.solution_list,
                            'computing time': self.get_current_computing_time()}
+
         self.observable.notify_all(**observable_data)
 
     def is_stopping_condition_reached(self) -> bool:
@@ -75,54 +80,52 @@ class SMPSO(ParticleSwarmOptimization):
 
     def create_initial_swarm(self) -> List[FloatSolution]:
         swarm = []
-
-        for i in range(self.swarm_size):
+        for _ in range(self.swarm_size):
             swarm.append(self.problem.create_solution())
-
         return swarm
 
     def evaluate_swarm(self, swarm: List[FloatSolution]) -> List[FloatSolution]:
         return self.evaluator.evaluate(swarm, self.problem)
 
     def initialize_global_best(self, swarm: List[FloatSolution]) -> None:
-        for particle in self.swarm:
+        for particle in swarm:
             self.leaders.add(particle)
 
     def initialize_particle_best(self, swarm: List[FloatSolution]) -> None:
-        for particle in self.swarm:
+        for particle in swarm:
             particle.attributes["local_best"] = copy(particle)
 
     def initialize_velocity(self, swarm: List[FloatSolution]) -> None:
-        pass # Velocity initialized in the constructor
+        pass  # Velocity initialized in the constructor
 
     def update_velocity(self, swarm: List[FloatSolution]) -> None:
         for i in range(self.swarm_size):
-            particle = copy(self.swarm[i])
-            best_particle = copy(self.swarm[i].attributes["local_best"])
+            best_particle = copy(swarm[i].attributes["local_best"])
             best_global = self.__select_global_best()
 
-            r1 = Random.random()
-            r2 = Random.random()
-
-            c1 = Random.uniform(self.c1_min, self.c1_max)
-            c2 = Random.uniform(self.c2_min, self.c2_max)
-
-            wmin = self.min_weight
+            r1 = round(random.uniform(self.r1_min, self.r1_max), 1)
+            r2 = round(random.uniform(self.r2_min, self.r2_max), 1)
+            c1 = round(random.uniform(self.c1_min, self.c1_max), 1)
+            c2 = round(random.uniform(self.c2_min, self.c2_max), 1)
             wmax = self.max_weight
+            wmin = self.min_weight
 
-            for var in range(self.problem.number_of_variables):
+            for var in range(swarm[i].number_of_variables):
                 self.speed[i][var] = \
-                    self.__velocity_constriction(self.__constriction_coefficient(c1, c2) * \
-                                                 (wmax * self.speed[i][var] +
-                                                  c1 * r1 * (best_particle.variables[var] - particle.variables[var]) +
-                                                  c2 * r2 * (best_global.variables[var] - particle.variables[var])),
-                                                 var)
+                    self.__velocity_constriction(
+                        self.__constriction_coefficient(c1, c2) *
+                        ((self.__inertia_weight(self.evaluations, self.max_evaluations, wmax, wmin)
+                         * self.speed[i][var])
+                         + (c1 * r1 * (best_particle.variables[var] - swarm[i].variables[var]))
+                         + (c2 * r2 * (best_global.variables[var] - swarm[i].variables[var]))
+                         ),
+                        self.delta_max, self.delta_min, var)
 
     def update_position(self, swarm: List[FloatSolution]) -> None:
         for i in range(self.swarm_size):
-            particle = self.swarm[i]
+            particle = swarm[i]
 
-            for j in particle.variables:
+            for j in range(particle.number_of_variables):
                 particle.variables[j] += self.speed[i][j]
 
                 if particle.variables[j] < self.problem.lower_bound[j]:
@@ -134,30 +137,27 @@ class SMPSO(ParticleSwarmOptimization):
                     self.speed[i][j] *= self.change_velocity2
 
     def perturbation(self, swarm: List[FloatSolution]) -> None:
-        for particle in self.swarm:
-            self.mutation.execute(particle)
+        for i in range(self.swarm_size):
+            if (i % 6) == 0:
+                self.mutation.execute(swarm[i])
 
     def update_global_best(self, swarm: List[FloatSolution]) -> None:
-        for particle in self.swarm:
+        for particle in swarm:
             self.leaders.add(copy(particle))
 
     def update_particle_best(self, swarm: List[FloatSolution]) -> None:
         for i in range(self.swarm_size):
             flag = self.dominance_comparator.compare(
-                self.swarm[i],
-                self.swarm[i].attribute["local_best"])
-
-            if flag is not 1:
-                swarm[i].attributes["local_best"] = copy(self.swarm[i])
+                swarm[i],
+                swarm[i].attributes["local_best"])
+            if flag != 1:
+                swarm[i].attributes["local_best"] = copy(swarm[i])
 
     def get_result(self) -> List[FloatSolution]:
-        self.leaders.solution_list
+        return self.leaders.solution_list
 
     def __select_global_best(self) -> FloatSolution:
-        #pos1 = Random.randint(0, len(self.leaders.solution_list) - 1)
-        #pos2 = Random.randint(0, len(self.leaders.solution_list) - 1)
-        best_global = None
-        particles = Random.sample(self.leaders.solution_list, 2)
+        particles = random.sample(self.leaders.solution_list, 2)
         if self.leaders.get_comparator().compare(particles[0], particles[1]) < 1:
             best_global = copy(particles[0])
         else:
@@ -165,22 +165,23 @@ class SMPSO(ParticleSwarmOptimization):
 
         return best_global
 
-    def __velocity_constriction(self, value: float, variable_index: int) -> float:
-        result = None
-        if value > self.delta_max[variable_index]:
-            result = self.delta_max[variable_index]
-
-        if value < self.delta_min[variable_index]:
-            result = self.delta_min[variable_index]
+    def __velocity_constriction(self, value: float, delta_max: [], delta_min: [], variable_index: int) -> float:
+        result = value
+        if value > delta_max[variable_index]:
+            result = delta_max[variable_index]
+        if value < delta_min[variable_index]:
+            result = delta_min[variable_index]
 
         return result
 
+    def __inertia_weight(self, evaluations: int, max_evaluations: int, wmax: float, wmin: float):
+        return wmax
+
     def __constriction_coefficient(self, c1: float, c2: float) -> float:
-        result = 0.0
         rho = c1 + c2
         if rho <= 4:
             result = 1.0
         else:
-            result = 2.0 / (2.0 - rho - numpy.sqrt(pow(rho, 2.0) - 4.0 * rho))
+            result = 2.0 / (2.0 - rho - sqrt(pow(rho, 2.0) - 4.0 * rho))
 
         return result
--- jmetal/runner/multiobjective/smpso_standard_settings.py
+++ jmetal/runner/multiobjective/smpso_standard_settings.py
@@ -2,9 +2,10 @@ import logging
 
 from jmetal.algorithm.multiobjective.smpso import SMPSO
 from jmetal.component.archive import CrowdingDistanceArchive
+from jmetal.component.observer import AlgorithmObserver
 from jmetal.core.solution import FloatSolution
 from jmetal.operator.mutation import Polynomial
-from jmetal.problem.multiobjective.unconstrained import Kursawe
+from jmetal.problem.multiobjective.zdt import ZDT1
 from jmetal.util.solution_list_output import SolutionListOutput
 
 logging.basicConfig(level=logging.INFO)
@@ -12,21 +13,25 @@ logger = logging.getLogger(__name__)
 
 
 def main() -> None:
-    problem = Kursawe()
+    problem = ZDT1()
     algorithm = SMPSO(
         problem=problem,
         swarm_size=100,
         max_evaluations=25000,
-        mutation=Polynomial(1.0/problem.number_of_variables, distribution_index=20),
+        mutation=Polynomial(probability=1.0/problem.number_of_variables, distribution_index=20),
         leaders=CrowdingDistanceArchive(100))
+
+    observer = AlgorithmObserver(animation_speed=1*10e-8)
+    algorithm.observable.register(observer=observer)
 
     algorithm.run()
     result = algorithm.get_result()
 
-    SolutionListOutput[FloatSolution].print_function_values_to_file("FUN."+problem.get_name(), result)
+    SolutionListOutput[FloatSolution].plot_scatter_to_screen(result)
 
     logger.info("Algorithm (continuous problem): " + algorithm.get_name())
     logger.info("Problem: " + problem.get_name())
+
 
 if __name__ == '__main__':
     main()
