2d22390a10ac67b8ca7c1557080bde136481c6b8
benhid <makishinake@hotmail.com>
2018-05-04 13:05:31 +0200

    Updated doc configuration file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,31 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+# http://read-the-docs.readthedocs.org/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import Mock as MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+
+MOCK_MODULES = [
+    'numpy',
+    'scipy',
+    'matplotlib',
+    'scipy.ndimage',
+    'scipy.ndimage.filters',
+    'skimage',
+    'skimage.morphology',
+    'skimage.io',
+    'skimage.exposure',
+]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 
