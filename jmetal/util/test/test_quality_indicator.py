4857268f25cb73345598baaca07ab46e22b8e58b
ajnebro <antonio@lcc.uma.es>
2018-05-09 09:50:41 +0200

    Add new test case in file test_quality_indicator.py

--- jmetal/util/test/test_quality_indicator.py
+++ jmetal/util/test/test_quality_indicator.py
@@ -1,5 +1,6 @@
 import unittest
 
+from jmetal.util.FrontUtils import read_front_from_file
 from jmetal.util.quality_indicator import HyperVolume
 
 
@@ -8,7 +9,7 @@ class HyperVolumeTestCases(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_should_hyper_volume_return_5_0(self):
+    def test_should_hypervolume_return_5_0(self):
         reference_point = [2, 2, 2]
         front = [[1, 0, 1], [0, 1, 0]]
 
@@ -16,6 +17,16 @@ class HyperVolumeTestCases(unittest.TestCase):
         value = hv.compute(front)
 
         self.assertEqual(5.0, value)
+
+    def test_should_hypervolume_return_the_correct_value_when_applied_to_the_ZDT1_reference_front(self):
+        reference_point = [1, 1]
+        front = read_front_from_file("../../../resources/data/ZDT1.pf")
+
+        hv = HyperVolume(reference_point)
+        value = hv.compute(front)
+
+        self.assertAlmostEqual(0.666, value, delta=0.001)
+
 
 if __name__ == '__main__':
     unittest.main()
