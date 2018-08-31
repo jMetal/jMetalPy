cebe2b76718b8ee4b140b35962f9bdcf71601217
Rod <moar820326@gmail.com>
2018-08-31 18:40:57 -0400

    Update __init__.py
    Fix problem when importing class RandomSearch for peforming an experiment

--- jmetal/algorithm/__init__.py
+++ jmetal/algorithm/__init__.py
@@ -1,9 +1,10 @@
 from .multiobjective.nsgaii import NSGAII
 from .multiobjective.smpso import SMPSO, SMPSORP
 from .singleobjective.evolutionaryalgorithm import ElitistEvolutionStrategy, NonElitistEvolutionStrategy
+from .multiobjective.randomSearch import RandomSearch
 
 __all__ = [
     'NSGAII',
     'SMPSO', 'SMPSORP',
-    'ElitistEvolutionStrategy', 'NonElitistEvolutionStrategy'
+    'ElitistEvolutionStrategy', 'NonElitistEvolutionStrategy','RandomSearch'
 ]
