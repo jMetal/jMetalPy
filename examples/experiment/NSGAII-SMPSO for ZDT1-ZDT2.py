7d2ac72a2f257e58919da0b032b8cc6f422fc915
benhid <makishinake@hotmail.com>
2018-11-13 12:43:04 +0100

    Working on Lab for running experiments

---
+++ examples/experiment/NSGAII-SMPSO for ZDT1-ZDT2.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+from jmetal.algorithm import NSGAII, SMPSO
+from jmetal.component.quality_indicator import HyperVolume2
+from jmetal.operator import NullMutation, SBX, BinaryTournamentSelection, Polynomial
+from jmetal.problem import ZDT1, ZDT6
+from jmetal.component import CrowdingDistanceArchive, RankingAndCrowdingDistanceComparator, HyperVolume
+from jmetal.util.laboratory import Experiment
+
+
+# Configure experiment
+problem_list = [ZDT1(), ZDT6()]
+metric_list = [HyperVolume(reference_point=[1, 1]), HyperVolume2(reference_point=[1, 1])]
+algorithm_list = []
+
+for problem in problem_list:
+    algorithm_list.append({
+        'label': 'NSGAII_A',
+        'algorithm': NSGAII(
+             problem=problem,
+             population_size=100,
+             max_evaluations=10000,
+             mutation=NullMutation(),
+             crossover=SBX(probability=1.0, distribution_index=20),
+             selection=BinaryTournamentSelection(comparator=RankingAndCrowdingDistanceComparator())
+        )}
+    )
+    algorithm_list.append({
+        'label': 'NSGAII_B',
+        'algorithm': NSGAII(
+             problem=problem,
+             population_size=100,
+             max_evaluations=10000,
+             mutation=Polynomial(probability=1.0 / problem.number_of_variables, distribution_index=20),
+             crossover=SBX(probability=1.0, distribution_index=20),
+             selection=BinaryTournamentSelection(comparator=RankingAndCrowdingDistanceComparator())
+        )}
+    )
+    algorithm_list.append({
+        'label': 'SMPSO',
+        'algorithm': SMPSO(
+             problem=problem,
+             swarm_size=100,
+             max_evaluations=5000,
+             mutation=Polynomial(probability=0.5, distribution_index=20),
+             leaders=CrowdingDistanceArchive(100)
+        )}
+    )
+
+study = Experiment(base_directory='./ex', algorithm_list=algorithm_list, problem_list=problem_list,
+                   metric_list=metric_list, n_runs=1)
+study.run()
+results = study.compute_metrics()
+
+print(results)
+
+mean_results = results.groupby(['problem', 'metric']).mean()
+median_results = results.groupby(['problem', 'metric']).median()
+
+print(mean_results)
+print(median_results)
+# print(results.groupby(['problem', 'metric']).min())
+# print(results.groupby(['problem', 'metric']).max())
+print(mean_results.xs('Hypervolume', level='metric'))
+
+print(study.convert_to_latex(mean_results.xs('Hypervolume', level='metric')))
--- jmetal/util/laboratory.py
+++ jmetal/util/laboratory.py
@@ -1,5 +1,10 @@
+import io
 import logging
 from concurrent.futures import ProcessPoolExecutor
+from itertools import chain
+
+from scipy import stats
+import pandas as pd
 
 jMetalPyLogger = logging.getLogger('jMetalPy')
 
@@ -14,55 +19,130 @@ jMetalPyLogger = logging.getLogger('jMetalPy')
 
 class Experiment:
 
-    def __init__(self, algorithm_list: list, n_runs: int = 1, m_workers: int = 6):
-        """ :param algorithm_list: List of algorithms as Tuple(Algorithm, dic() with parameters).
-        :param m_workers: Maximum number of workers for ProcessPoolExecutor. """
+    def __init__(self, base_directory: str, algorithm_list: list, problem_list: list, metric_list: list,
+                 n_runs: int = 1, m_workers: int = 6):
+        """ Run an experiment to evaluate algorithms and/or problems.
+
+        :param base_directory: Directory to save partial outputs.
+        :param algorithm_list: List of algorithms as Tuple(Algorithm, dic() with parameters).
+        :param metric_list: List of metrics. Each metric should inherit from :py:class:`Metric` or, at least,
+        contain a method `compute`.
+        :param m_workers: Maximum number of workers for ProcessPoolExecutor.
+        """
+        self.base_dir = base_directory
+
         self.algorithm_list = algorithm_list
+        self.problem_list = problem_list
+        self.metric_list = metric_list
+        self.experiment_list = list()
 
         self.n_runs = n_runs
         self.m_workers = m_workers
-        self.experiment_list = list()
 
     def run(self) -> None:
-        """ Run the experiment. """
-        self.__configure_algorithm_list()
+        self.__setup_experiments()
 
         with ProcessPoolExecutor(max_workers=self.m_workers) as pool:
-            for name, algorithm, n_run in self.experiment_list:
-                jMetalPyLogger.info('Running experiment {0}'.format(
-                    name, algorithm.problem, n_run)
-                )
-
+            for label, algorithm, n_run in self.experiment_list:
+                # algorithm.observable.register(observer=WriteFrontToFileObserver(output_directory=self.base_dir + label))
                 pool.submit(algorithm.run())
 
             # Wait until all computation is done for this problem
-            jMetalPyLogger.debug('Waiting')
             pool.shutdown(wait=True)
 
-    def compute_metrics(self, metric_list: list) -> dict:
-        """ :param metric_list: List
-        of metrics. Each metric should inherit from :py:class:`Metric` or, at least, contain a method `compute`. """
-        results = dict()
+    def compute_metrics(self) -> pd.DataFrame:
+        runs = list(range(0, self.n_runs)) * len(self.metric_list) * len(self.problem_list)
+        metrics = [[metric.get_name()] * self.n_runs for metric in self.metric_list] * len(self.problem_list)
+        problems = [[problem.get_name()] * self.n_runs * len(self.metric_list) for problem in self.problem_list]
 
-        for name, algorithm, n_run in self.experiment_list:
-            results[name] = {}
+        arrays = [list(chain(*problems)), list(chain(*metrics)), runs]
+        index = pd.MultiIndex.from_arrays(arrays, names=['problem', 'metric', 'run'])
+        df = pd.DataFrame(data=None, columns=set([c['label'] for c in self.algorithm_list]), index=index, dtype=float)
 
-            for metric in metric_list:
-                results[name].setdefault('metric', dict()).update(
-                    {metric.get_name(): metric.compute(algorithm.get_result())}
-                )
+        for label, algorithm, n_run in self.experiment_list:
+            for metric in self.metric_list:
+                r = metric.compute(algorithm.get_result())
+                df.loc[(algorithm.problem.get_name(), metric.get_name(), n_run), label] = r
 
-        return results
+        return df
 
-    def export_to_file(self, base_directory: str = 'experiment', function_values_filename: str = 'FUN',
-                       variables_filename: str = 'VAR'):
-        for tag, algorithm, run in self.experiment_list:
-            # todo Save VAR and FUN to files
-            pass
+    def compute_statistical_analysis(self, data: pd.DataFrame):
+        """ Compute the mean and standard deviation, median and interquartile range.
 
-    def __configure_algorithm_list(self):
-        """ Configure the algorithm list, by making a triple of (name, algorithm, run). """
+        :param data_list: List of data sets.
+        """
+        pass
+
+    def compute_statiscal_analysis_2(self, data_list: list):
+        """ :param data_list: List of data sets.
+
+        The application scheme listed here is as described in
+
+        * G. Luque, E. Alba, Parallel Genetic Algorithms, Springer-Verlag, ISBN 978-3-642-22084-5, 2011
+        """
+        if len(data_list) < 2:
+            raise Exception('Data sets number must be equal or greater than two')
+
+        dt = pd.DataFrame(data=None,
+                          columns=[algorithm[0] for algorithm in self.algorithm_list],
+                          index=[problem.get_name() for problem in self.problem_list])
+
+        normality_test = True
+
+        for data in data_list:
+            statistic, pvalue = stats.kstest(data, 'norm')
+
+            if pvalue > 0.05:
+                normality_test = False
+                break
+
+        if not normality_test:
+            # non-normal variables (median comparison, non-parametric tests)
+            if len(data_list) == 2:
+                statistic, pvalue = stats.wilcoxon(data_list[0], data_list[1])
+            else:
+                statistic, pvalue = stats.kruskal(*data_list)
+        else:
+            # normal variables (mean comparison, parametric tests)
+            if len(data_list) == 2:
+                pass
+            else:
+                pass
+
+    def __setup_experiments(self):
+        """ Configure the algorithm list, by making a triple of (label, algorithm, n_run). """
         for n_run in range(self.n_runs):
-            for tag, algorithm in self.algorithm_list:
-                name = '{0}.{1}.{2}'.format(tag, algorithm.problem.get_name(), n_run)
-                self.experiment_list.append((name, algorithm, n_run))
+            for configuration in self.algorithm_list:
+                self.experiment_list.append((configuration['label'], configuration['algorithm'], n_run))
+
+    def convert_to_latex(self, df: pd.DataFrame, caption: str='Experiment', label: str='tab:exp', alignment: str='c'):
+        """ Convert a pandas dataframe to a LaTeX tabular. Prints labels in bold, does not use math mode. """
+        num_columns, num_rows = df.shape[1], df.shape[0]
+        output = io.StringIO()
+
+        col_format = '{}|{}'.format(alignment, alignment * num_columns)
+        column_labels = ['\\textbf{{{0}}}'.format(label.replace('_', '\\_')) for label in df.columns]
+
+        # Write header
+        output.write('\\begin{table}\n')
+        output.write('\\caption{{{}}}\n'.format(caption))
+        output.write('\\label{{{}}}\n'.format(label))
+        output.write('\\centering\n')
+        output.write('\\begin{scriptsize}\n')
+        output.write('\\begin{tabular}{%s}\n' % col_format)
+        output.write('\\hline\n')
+        output.write('& {} \\\\\\hline\n'.format(' & '.join(column_labels)))
+
+        # Write data lines
+        for i in range(num_rows):
+            output.write('\\textbf{{{0}}} & ${1}$ \\\\\n'.format(
+                df.index[i], '$ & $'.join([str(val) for val in df.ix[i]]))
+            )
+
+        # Write footer
+        output.write('\\hline\n')
+        output.write('\\end{tabular}\n')
+        output.write('\\end{scriptsize}\n')
+        output.write('\\end{table}')
+
+        return output.getvalue()
