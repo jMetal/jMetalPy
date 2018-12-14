e701e669e93bcb5d9da876d7a87ec37f71a8d726
benhid <makishinake@hotmail.com>
2018-12-14 10:57:34 +0100

    Added example for NSGA-II solving the ZDT1 problem

---
+++ examples/multiobjective/nsgaii_zdt1.py
@@ -0,0 +1,28 @@
+from jmetal.algorithm.multiobjective.nsgaii import NSGAII
+from jmetal.component import RankingAndCrowdingDistanceComparator
+from jmetal.operator import SBX, Polynomial, BinaryTournamentSelection
+from jmetal.problem import ZDT1
+from jmetal.util.solution_list import read_solutions
+from jmetal.util.termination_criteria import StoppingByEvaluations
+
+if __name__ == '__main__':
+    problem = ZDT1()
+    problem.reference_front = read_solutions(file_path='../../resources/reference_front/{}.pf'.format(problem.get_name()))
+
+    algorithm = NSGAII(
+        problem=problem,
+        population_size=100,
+        offspring_size=100,
+        mating_pool_size=100,
+        mutation=Polynomial(probability=1.0 / problem.number_of_variables, distribution_index=20),
+        crossover=SBX(probability=1.0, distribution_index=20),
+        selection=BinaryTournamentSelection(comparator=RankingAndCrowdingDistanceComparator()),
+        termination_criteria=StoppingByEvaluations(max=25000)
+    )
+
+    algorithm.run()
+    front = algorithm.get_result()
+
+    print('Algorithm (continuous problem): ' + algorithm.get_name())
+    print('Problem: ' + problem.get_name())
+    print('Computing time: ' + str(algorithm.total_computing_time))
