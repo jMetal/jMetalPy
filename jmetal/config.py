8d5623893bd9a0eb30c58b0e9d47ba8b8fe4ee0f
benhid <makishinake@hotmail.com>
2018-12-17 13:12:15 +0100

    Working on default operators for each problem type

--- jmetal/config.py
+++ jmetal/config.py
@@ -1,3 +1,5 @@
+from jmetal.operator import Polynomial, BitFlip
+
 from jmetal.component import SequentialEvaluator, RandomGenerator, DefaultObservable
 from jmetal.util.termination_criteria import StoppingByEvaluations
 
@@ -10,6 +12,10 @@ class _Store(object):
         self.default_evaluator = SequentialEvaluator()
         self.default_generator = RandomGenerator()
         self.default_termination_criteria = StoppingByEvaluations(max=25000)
+        self.default_mutation = {
+            'real': Polynomial(probability=0.15, distribution_index=20),
+            'binary': BitFlip(0.15)
+        }
 
 
 store = _Store()
