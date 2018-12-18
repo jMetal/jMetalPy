2d9084b19f4c11fb84e0d338a9fe6914fcfba208
ajnebro <antonio@lcc.uma.es>
2018-12-18 18:09:13 +0100

    G-NSGA-II working (but the progress bar appears twice

--- examples/multiobjective/gnsgaii_zdt2.py
+++ examples/multiobjective/gnsgaii_zdt2.py
@@ -1,5 +1,5 @@
 from jmetal.algorithm.multiobjective.nsgaii import NSGAII
-from jmetal.component import RankingAndCrowdingDistanceComparator
+from jmetal.component import RankingAndCrowdingDistanceComparator, ProgressBarObserver, VisualizerObserver
 from jmetal.component.comparator import GDominanceComparator
 from jmetal.operator import SBX, Polynomial, BinaryTournamentSelection
 from jmetal.problem import ZDT2
@@ -21,26 +21,32 @@ if __name__ == '__main__':
 
     reference_point = [0.5, 0.5]
 
+    max_evaluations = 25000
     algorithm = NSGAII(
         problem=problem,
         population_size=100,
-        offspring_size=100,
+        offspring_population_size=100,
         mating_pool_size=100,
         mutation=Polynomial(probability=1.0 / problem.number_of_variables, distribution_index=20),
         crossover=SBX(probability=1.0, distribution_index=20),
         selection=BinaryTournamentSelection(comparator=RankingAndCrowdingDistanceComparator()),
         dominance_comparator=GDominanceComparator(reference_point),
-        termination_criteria=StoppingByEvaluations(max=25000),
+        termination_criterion=StoppingByEvaluations(max=max_evaluations),
         # termination_criteria=StoppingByTime(max_seconds=20),
         # termination_criteria=StoppingByQualityIndicator(quality_indicator=HyperVolume([1.0, 1.0]), expected_value=0.5, degree=0.95)
     )
 
+    progress_bar = ProgressBarObserver(max=max_evaluations)
+    algorithm.observable.register(observer=progress_bar)
+    algorithm.observable.register(observer=VisualizerObserver())
+
     algorithm.run()
     front = algorithm.get_result()
 
-    # Save variables to file
-    print_function_values_to_file(front, 'FUN.GNSGAII.ZDT2')
-    print_variables_to_file(front, 'VAR.GNSGAII.ZDT2')
+    # Save results to file
+    print_function_values_to_file(front, 'FUN.' + algorithm.get_name() + "." + problem.get_name())
+    print_variables_to_file(front, 'VAR.'+ algorithm.get_name() + "." + problem.get_name())
+
 
     print('Algorithm (continuous problem): ' + algorithm.get_name())
     print('Problem: ' + problem.get_name())
