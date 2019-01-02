27e5e28b906195e8ee8b4afdc44b5fa62227e68c
ajnebro <antonio@lcc.uma.es>
2019-01-02 10:11:31 +0100

    Add program smpso_srinivas_on_the_fly.py

---
+++ examples/multiobjective/smpso_srinivas_on_the_fly.py
@@ -0,0 +1,57 @@
+from jmetal.algorithm.multiobjective.smpso import SMPSO
+from jmetal.component import ProgressBarObserver, CrowdingDistanceArchive, VisualizerObserver
+from jmetal.core.problem import OnTheFlyFloatProblem
+from jmetal.operator import Polynomial
+from jmetal.problem import DTLZ1, ZDT4, Srinivas
+from jmetal.util.graphic import InteractivePlot
+from jmetal.util.solution_list import print_function_values_to_file, print_variables_to_file
+from jmetal.util.termination_criterion import StoppingByEvaluations
+
+if __name__ == '__main__':
+
+    # Defining problem Srinivas on the fly
+    def f1(x:[float]):
+        return 2.0 + (x[0] - 2.0) * (x[0] - 2.0) + (x[1] - 1.0) * (x[1] - 1.0)
+
+    def f2(x:[float]):
+        return 9.0 * x[0] - (x[1] - 1.0) * (x[1] - 1.0)
+
+    def c1(x:[float]):
+        return 1.0 - (x[0] * x[0] + x[1] * x[1]) / 225.0
+
+    def c2(x:[float]):
+        return (3.0 * x[1] - x[0]) / 10.0 - 1.0
+
+    problem = OnTheFlyFloatProblem()
+    problem\
+        .set_name("Srinivas")\
+        .add_variable(-20.0, 20.0)\
+        .add_variable(-20.0, 20.0)\
+        .add_function(f1)\
+        .add_function(f2)\
+        .add_constraint(c1)\
+        .add_constraint(c2)
+
+    max_evaluations = 25000
+    algorithm = SMPSO(
+        problem=problem,
+        swarm_size=100,
+        mutation=Polynomial(probability=1.0 / problem.number_of_variables, distribution_index=20),
+        leaders=CrowdingDistanceArchive(100),
+        termination_criterion=StoppingByEvaluations(max=max_evaluations)
+    )
+
+    progress_bar = ProgressBarObserver(max=max_evaluations)
+    algorithm.observable.register(observer=progress_bar)
+    algorithm.observable.register(observer=VisualizerObserver())
+
+    algorithm.run()
+    front = algorithm.get_result()
+
+    # Save results to file
+    print_function_values_to_file(front, 'FUN.' + algorithm.get_name() + "." + problem.get_name())
+    print_variables_to_file(front, 'VAR.'+ algorithm.get_name() + "." + problem.get_name())
+
+    print('Algorithm (continuous problem): ' + algorithm.get_name())
+    print('Problem: ' + problem.get_name())
+    print('Computing time: ' + str(algorithm.total_computing_time))
