111f1745ca18d233cf5fa934648594b581c52c2b
benhid <makishinake@hotmail.com>
2019-01-11 13:37:34 +0100

    Moved file

--- examples/experiment/statistical_analysis.py
+++ examples/experiment/statistical_analysis.py
@@ -6,8 +6,8 @@ from jmetal.util.statistical_test.functions import *
 if __name__ == '__main__':
     base_directory = 'data'
 
-    compute_median_iqr_tables(filename='data/QualityIndicatorSummary2.csv')
-    avg = compute_mean_indicator(filename='data/QualityIndicatorSummary2.csv', indicator_name='HV')
+    compute_median_iqr_tables(filename='QualityIndicatorSummary.csv')
+    avg = compute_mean_indicator(filename='QualityIndicatorSummary.csv', indicator_name='HV')
 
     print(avg)
 
