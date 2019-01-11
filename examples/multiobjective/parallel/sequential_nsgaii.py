c921a3204171a28e5333816f9af4dcdfd0fc2b76
ajnebro <ajnebro@outlook.com>
2019-01-11 11:48:06 +0100

    Reorganize examples

--- examples/multiobjective/parallel/sequential_nsgaii.py
+++ examples/multiobjective/parallel/sequential_nsgaii.py
@@ -1,10 +1,12 @@
-from examples.multiobjective.distributed_nsgaII import ZDT1Modified
+from examples.multiobjective.parallel.zdt1_modified import ZDT1Modified
 from jmetal.algorithm.multiobjective.nsgaii import NSGAII
-from jmetal.util.comparator import RankingAndCrowdingDistanceComparator
-from jmetal.util.observer import ProgressBarObserver
-from jmetal.util.solution_list.evaluator import MapEvaluator, DaskEvaluator
 from jmetal.operator import SBXCrossover, PolynomialMutation, BinaryTournamentSelection
+from jmetal.problem import ZDT1
+from jmetal.util.comparator import RankingAndCrowdingDistanceComparator
+from jmetal.util.observer import ProgressBarObserver, VisualizerObserver
+from jmetal.util.solution_list import read_solutions, print_function_values_to_file, print_variables_to_file
 from jmetal.util.termination_criterion import StoppingByEvaluations
+from jmetal.util.visualization import Plot
 
 if __name__ == '__main__':
     problem = ZDT1Modified()
@@ -12,7 +14,6 @@ if __name__ == '__main__':
     max_evaluations = 100
 
     algorithm = NSGAII(
-        population_evaluator=DaskEvaluator(),
         problem=problem,
         population_size=10,
         offspring_population_size=10,
@@ -21,13 +22,14 @@ if __name__ == '__main__':
         selection=BinaryTournamentSelection(comparator=RankingAndCrowdingDistanceComparator()),
         termination_criterion=StoppingByEvaluations(max=max_evaluations)
     )
-    progress_bar = ProgressBarObserver(max=max_evaluations)
-    algorithm.observable.register(observer=progress_bar)
 
     algorithm.run()
     front = algorithm.get_result()
 
+    # Save results to file
+    print_function_values_to_file(front, 'FUN.NSGAII.ZDT1')
+    print_variables_to_file(front, 'VAR.NSGAII.ZDT1')
+
     print('Algorithm (continuous problem): ' + algorithm.get_name())
     print('Problem: ' + problem.get_name())
     print('Computing time: ' + str(algorithm.total_computing_time))
-
--- examples/multiobjective/parallel/zdt1_modified.py
+++ examples/multiobjective/parallel/zdt1_modified.py
@@ -1,15 +1,7 @@
 from math import sqrt
-from multiprocessing.pool import ThreadPool
 
-import dask
-from dask.distributed import Client
-
-from jmetal.algorithm.multiobjective.nsgaii import DistributedNSGAII
 from jmetal.core.problem import FloatProblem
 from jmetal.core.solution import FloatSolution
-from jmetal.operator import PolynomialMutation, SBXCrossover, BinaryTournamentSelection
-from jmetal.util.comparator import RankingAndCrowdingDistanceComparator
-from jmetal.util.observer import ProgressBarObserver
 
 
 class ZDT1Modified(FloatProblem):
@@ -60,30 +52,3 @@ class ZDT1Modified(FloatProblem):
 
     def get_name(self):
         return 'ZDT11'
-
-
-if __name__ == '__main__':
-    problem = ZDT1Modified()
-
-    dask.config.set(scheduler='threads', pool=ThreadPool(8))
-    client = Client()
-
-    algorithm = DistributedNSGAII(
-        problem=problem,
-        population_size=10,
-        max_evaluations=100,
-        mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=20),
-        crossover=SBXCrossover(probability=1.0, distribution_index=20),
-        selection=BinaryTournamentSelection(comparator=RankingAndCrowdingDistanceComparator()),
-        number_of_cores=8,
-        client=client
-    )
-
-    algorithm.observable.register(observer=ProgressBarObserver(max=100))
-
-    algorithm.run()
-    front = algorithm.get_result()
-
-    print('Algorithm: ' + algorithm.get_name())
-    print('Problem: ' + problem.get_name())
-
