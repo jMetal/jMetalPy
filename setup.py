612f391751610a031868a06889bab086fc294c60
benhid <atbnhd@gmail.com>
2019-07-01 10:54:41 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-from setuptools import find_packages
-from setuptools import setup
+from os.path import abspath, dirname, join
+
+from setuptools import find_packages, setup
+
+basedir = abspath(dirname(__file__))
+
+with open(join(basedir, 'README.md'), encoding='utf-8') as f:
+    README = f.read()
 
 setup(
     name='jmetalpy',
     version='1.0.5',
     description='Python version of the jMetal framework',
+    long_description=README,
+    long_description_content_type='text/markdown',
     author='Antonio J. Nebro',
     author_email='antonio@lcc.uma.es',
     maintainer='Antonio J. Nebro, Antonio Benitez-Hidalgo',
     maintainer_email='antonio@lcc.uma.es, antonio.b@uma.es',
     license='MIT',
     url='https://github.com/jMetal/jMetalPy',
-    long_description=open('README.md').read(),
     packages=find_packages(exclude=['test_']),
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -22,16 +29,16 @@ setup(
     ],
     install_requires=[
         'tqdm',
-        'numpy>=1.16.0',
-        'pandas>=0.23.4',
-        'scipy==1.1.0',
-        'pyspark>=2.4.0',
         'ipython',
-        'holoviews>=1.10.9',
+        'scipy==1.1.0',
+        'numpy>=1.16.0',
+        'pandas>=0.24.2',
+        'pyspark>=2.4.0',
         'plotly>=3.3.0',
         'matplotlib>=3.0.2',
         'statsmodels>=0.9.0',
-        'dask[complete]==1.2.2'
+        'dask[complete]==1.2.2',
+        'distributed==1.28.1'
     ],
     tests_require=[
         'mockito',
