7fb5baec209e8aab1b9fd35b4333c2d9a2acd874
ajnebro <ajnebro@outlook.com>
2019-12-16 20:39:37 +0100

    Add method get_non_dominated_solutions() to file helper.py

--- jmetal/util/solutions/helper.py
+++ jmetal/util/solutions/helper.py
@@ -4,9 +4,10 @@ from pathlib import Path
 from typing import List
 
 from jmetal.core.solution import FloatSolution, Solution
-#from jmetal.util.archive import NonDominatedSolutionListArchive
 
 LOGGER = logging.getLogger('jmetal')
+
+from jmetal.util.archive import NonDominatedSolutionListArchive, BoundedArchive
 
 """
 .. module:: solutions
@@ -17,13 +18,13 @@ LOGGER = logging.getLogger('jmetal')
 """
 
 
-#def get_non_dominated_solutions(solutions: List[Solution]) -> List[Solution]:
-#    archive: NonDominatedSolutionListArchive()
-#
-#    for solution in solutions:
-#        archive.add(solution)
-#
-#    return archive.solution_list
+def get_non_dominated_solutions(solutions: List[Solution]) -> List[Solution]:
+    archive: BoundedArchive = NonDominatedSolutionListArchive()
+
+    for solution in solutions:
+        archive.add(solution)
+
+    return archive.solution_list
 
 
 def read_solutions(filename: str) -> List[FloatSolution]:
