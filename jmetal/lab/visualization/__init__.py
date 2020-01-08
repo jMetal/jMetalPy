0c7f28ab0ab17cd1c25ff222e53be8f659b5bbb5
benhid <atbnhd@gmail.com>
2020-01-08 14:16:03 +0100

    Delete macos import

--- jmetal/lab/visualization/__init__.py
+++ jmetal/lab/visualization/__init__.py
@@ -1,9 +1,3 @@
-try:
-    from matplotlib.backends import _macosx
-except ImportError:
-    import matplotlib
-    matplotlib.use('Qt5Agg')
-
 from jmetal.lab.statistical_test.critical_distance import CDplot
 from .interactive import InteractivePlot
 from .plotting import Plot
