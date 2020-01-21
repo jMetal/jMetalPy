76dddaeedcd61889c3b191424e778be49c567ba2
ajnebro <ajnebro@outlook.com>
2020-01-21 10:56:04 +0100

    Add class IntegerFloatSBXCrossover

--- jmetal/operator/crossover.py
+++ jmetal/operator/crossover.py
@@ -3,7 +3,8 @@ import random
 from typing import List
 
 from jmetal.core.operator import Crossover
-from jmetal.core.solution import Solution, FloatSolution, BinarySolution, PermutationSolution
+from jmetal.core.solution import Solution, FloatSolution, BinarySolution, PermutationSolution, IntegerFloatSolution, \
+    IntegerSolution
 
 """
 .. module:: crossover
@@ -216,6 +217,86 @@ class SBXCrossover(Crossover[FloatSolution, FloatSolution]):
         return 'SBX crossover'
 
 
+class IntegerSBXCrossover(Crossover[IntegerSolution, IntegerSolution]):
+    __EPS = 1.0e-14
+
+    def __init__(self, probability: float, distribution_index: float = 20.0):
+        super(IntegerSBXCrossover, self).__init__(probability=probability)
+        self.distribution_index = distribution_index
+
+    def execute(self, parents: List[IntegerSolution]) -> List[IntegerSolution]:
+        if len(parents) != 2:
+            raise Exception('The number of parents is not two: {}'.format(len(parents)))
+
+        offspring = [copy.deepcopy(parents[0]), copy.deepcopy(parents[1])]
+        rand = random.random()
+
+        if rand <= self.probability:
+            for i in range(parents[0].number_of_variables):
+                value_x1, value_x2 = parents[0].variables[i], parents[1].variables[i]
+
+                if random.random() <= 0.5:
+                    if abs(value_x1 - value_x2) > self.__EPS:
+                        if value_x1 < value_x2:
+                            y1, y2 = value_x1, value_x2
+                        else:
+                            y1, y2 = value_x2, value_x1
+
+                        lower_bound, upper_bound = parents[0].lower_bound[i], parents[1].upper_bound[i]
+
+                        beta = 1.0 + (2.0 * (y1 - lower_bound) / (y2 - y1))
+                        alpha = 2.0 - pow(beta, -(self.distribution_index + 1.0))
+
+                        rand = random.random()
+                        if rand <= (1.0 / alpha):
+                            betaq = pow(rand * alpha, (1.0 / (self.distribution_index + 1.0)))
+                        else:
+                            betaq = pow(1.0 / (2.0 - rand * alpha), 1.0 / (self.distribution_index + 1.0))
+
+                        c1 = 0.5 * (y1 + y2 - betaq * (y2 - y1))
+                        beta = 1.0 + (2.0 * (upper_bound - y2) / (y2 - y1))
+                        alpha = 2.0 - pow(beta, -(self.distribution_index + 1.0))
+
+                        if rand <= (1.0 / alpha):
+                            betaq = pow((rand * alpha), (1.0 / (self.distribution_index + 1.0)))
+                        else:
+                            betaq = pow(1.0 / (2.0 - rand * alpha), 1.0 / (self.distribution_index + 1.0))
+
+                        c2 = 0.5 * (y1 + y2 + betaq * (y2 - y1))
+
+                        if c1 < lower_bound:
+                            c1 = lower_bound
+                        if c2 < lower_bound:
+                            c2 = lower_bound
+                        if c1 > upper_bound:
+                            c1 = upper_bound
+                        if c2 > upper_bound:
+                            c2 = upper_bound
+
+                        if random.random() <= 0.5:
+                            offspring[0].variables[i] = int(c2)
+                            offspring[1].variables[i] = int(c1)
+                        else:
+                            offspring[0].variables[i] = int(c1)
+                            offspring[1].variables[i] = int(c2)
+                    else:
+                        offspring[0].variables[i] = value_x1
+                        offspring[1].variables[i] = value_x2
+                else:
+                    offspring[0].variables[i] = value_x1
+                    offspring[1].variables[i] = value_x2
+        return offspring
+
+    def get_number_of_parents(self) -> int:
+        return 2
+
+    def get_number_of_children(self) -> int:
+        return 2
+
+    def get_name(self) -> str:
+        return 'Integer SBX crossover'
+
+
 class SPXCrossover(Crossover[BinarySolution, BinarySolution]):
 
     def __init__(self, probability: float):
@@ -324,3 +405,34 @@ class DifferentialEvolutionCrossover(Crossover[FloatSolution, FloatSolution]):
 
     def get_name(self) -> str:
         return 'Differential Evolution crossover'
+
+
+class IntegerFloatSBXCrossover(Crossover[IntegerFloatSolution, IntegerFloatSolution]):
+    __EPS = 1.0e-14
+
+    def __init__(self, integer_SBX_crossover: IntegerSBXCrossover, float_SBX_crossover: SBXCrossover):
+        super(IntegerFloatSBXCrossover, self).__init__()
+        self.integer_SBX_crossover = integer_SBX_crossover
+        self.float_SBX_crossover = float_SBX_crossover
+
+    def execute(self, parents: List[IntegerFloatSolution]) -> List[IntegerFloatSolution]:
+        if len(parents) != 2:
+            raise Exception('The number of parents is not two: {}'.format(len(parents)))
+
+        integer_parent_solutions = [parents[0].variables[0], parents[1].variables[0]]
+        float_parent_solutions = [parents[0].variables[1], parents[1].variables[1]]
+
+        integer_children_solutions = self.integer_SBX_crossover.execute(integer_parent_solutions)
+        float_children_solutions = self.float_SBX_crossover.execute(float_parent_solutions)
+
+        return [IntegerFloatSolution(integer_children_solutions[0], float_children_solutions[0]),
+                IntegerFloatSolution(integer_children_solutions[1], float_children_solutions[1])]
+
+    def get_number_of_parents(self) -> int:
+        return 2
+
+    def get_number_of_children(self) -> int:
+        return 2
+
+    def get_name(self) -> str:
+        return 'Integer floatSBX crossover'
