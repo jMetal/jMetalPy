5b78cc7f389fc5168399a0bafb1e3b08e06380cc
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2020-12-16 15:29:48 +0100

    Update __init__.py
    Export `RouletteWheelSelection`

--- jmetal/operator/__init__.py
+++ jmetal/operator/__init__.py
@@ -1,13 +1,13 @@
 from .crossover import NullCrossover, SBXCrossover, SPXCrossover, DifferentialEvolutionCrossover
 from .mutation import NullMutation, BitFlipMutation, PolynomialMutation, IntegerPolynomialMutation, UniformMutation, \
     SimpleRandomMutation
-from .selection import BestSolutionSelection, BinaryTournamentSelection, BinaryTournament2Selection, \
+from .selection import RouletteWheelSelection, BestSolutionSelection, BinaryTournamentSelection, BinaryTournament2Selection, \
     RandomSolutionSelection, NaryRandomSolutionSelection, RankingAndCrowdingDistanceSelection
 
 __all__ = [
     'NullCrossover', 'SBXCrossover', 'SPXCrossover', 'DifferentialEvolutionCrossover',
     'NullMutation', 'BitFlipMutation', 'PolynomialMutation', 'IntegerPolynomialMutation', 'UniformMutation',
     'SimpleRandomMutation',
-    'BestSolutionSelection', 'BinaryTournamentSelection', 'BinaryTournament2Selection', 'RandomSolutionSelection',
+    'RouletteWheelSelection', 'BestSolutionSelection', 'BinaryTournamentSelection', 'BinaryTournament2Selection', 'RandomSolutionSelection',
     'NaryRandomSolutionSelection', 'RankingAndCrowdingDistanceSelection'
 ]
