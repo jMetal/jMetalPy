614347beb1fd1c65f3f479509e4fd04c508bd0f0
ajnebro <ajnebro@users.noreply.github.com>
2022-11-23 17:13:35 +0100

    Add comments

--- examples/experiment/generateMedianAndWilcoxonLatexTables.py
+++ examples/experiment/generateMedianAndWilcoxonLatexTables.py
@@ -3,5 +3,9 @@ from jmetal.lab.experiment import (
 )
 
 if __name__ == "__main__":
-    # Generate Median & IQR tables
+    """
+        Generate Latex tables including medians and IQRs. Additionally, the last algorithm is considered as the reference
+        algorithm, and the cells include a symbol indicating whether the differences with the reference algorithm
+        are significant or not according to the Wilcoxon rank sum test. 
+    """
     generate_median_and_wilcoxon_latex_tables(filename="QualityIndicatorSummary.csv")
