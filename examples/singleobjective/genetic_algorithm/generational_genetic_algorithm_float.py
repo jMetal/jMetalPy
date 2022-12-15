952199e559c24d62f69b69255f8f47689256db56
ajnebro <ajnebro@users.noreply.github.com>
2022-12-15 12:18:19 +0100

    Update example

--- examples/singleobjective/genetic_algorithm/generational_genetic_algorithm_float.py
+++ examples/singleobjective/genetic_algorithm/generational_genetic_algorithm_float.py
@@ -1,6 +1,7 @@
 from jmetal.algorithm.singleobjective.genetic_algorithm import GeneticAlgorithm
 from jmetal.operator import BinaryTournamentSelection, PolynomialMutation, SBXCrossover
 from jmetal.problem.singleobjective.unconstrained import Rastrigin
+from jmetal.util.comparator import ObjectiveComparator
 from jmetal.util.termination_criterion import StoppingByEvaluations
 
 if __name__ == "__main__":
@@ -10,10 +11,9 @@ if __name__ == "__main__":
         problem=problem,
         population_size=100,
         offspring_population_size=1,
-        mutation=PolynomialMutation(1.0 / problem.number_of_variables, 20.0),
+        mutation=PolynomialMutation(1.0 / problem.number_of_variables(), 20.0),
         crossover=SBXCrossover(0.9, 5.0),
-        selection=BinaryTournamentSelection(),
-        termination_criterion=StoppingByEvaluations(max_evaluations=100000),
+        termination_criterion=StoppingByEvaluations(max_evaluations=150000),
     )
 
     algorithm.run()
