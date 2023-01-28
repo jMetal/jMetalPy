ebd34b0b8e9d6c465da988a74b391b691784b2ab
ajnebro <ajnebro@users.noreply.github.com>
2023-01-28 17:23:10 +0100

    Refactor DTLZ problems

--- jmetal/problem/multiobjective/dtlz.py
+++ jmetal/problem/multiobjective/dtlz.py
@@ -21,40 +21,43 @@ class DTLZ1(FloatProblem):
     def __init__(self, number_of_variables: int = 7, number_of_objectives=3):
         """:param number_of_variables: number of decision variables of the problem."""
         super(DTLZ1, self).__init__()
-        self.number_of_variables = number_of_variables
-        self.number_of_objectives = number_of_objectives
-        self.number_of_constraints = 0
 
         self.obj_directions = [self.MINIMIZE] * number_of_objectives
         self.obj_labels = ["$ f_{} $".format(i) for i in range(number_of_objectives)]
 
-        self.lower_bound = self.number_of_variables * [0.0]
-        self.upper_bound = self.number_of_variables * [1.0]
+        self.lower_bound = number_of_variables * [0.0]
+        self.upper_bound = number_of_variables * [1.0]
+
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def number_of_constraints(self) -> int:
+        return 0
 
     def evaluate(self, solution: FloatSolution) -> FloatSolution:
-        k = self.number_of_variables - self.number_of_objectives + 1
+        k = self.number_of_variables() - self.number_of_objectives() + 1
 
         g = sum(
             [
                 (x - 0.5) * (x - 0.5) - cos(20.0 * pi * (x - 0.5))
-                for x in solution.variables[self.number_of_variables - k :]
+                for x in solution.variables[self.number_of_variables() - k :]
             ]
         )
 
         g = 100 * (k + g)
 
-        solution.objectives = [(1.0 + g) * 0.5] * self.number_of_objectives
+        solution.objectives = [(1.0 + g) * 0.5] * self.number_of_objectives()
 
-        for i in range(self.number_of_objectives):
-            for j in range(self.number_of_objectives - (i + 1)):
+        for i in range(self.number_of_objectives()):
+            for j in range(self.number_of_objectives() - (i + 1)):
                 solution.objectives[i] *= solution.variables[j]
 
             if i != 0:
-                solution.objectives[i] *= 1 - solution.variables[self.number_of_objectives - (i + 1)]
+                solution.objectives[i] *= 1 - solution.variables[self.number_of_objectives() - (i + 1)]
 
         return solution
 
-    def get_name(self):
+    def name(self):
         return "DTLZ1"
 
 
@@ -69,22 +72,22 @@ class DTLZ2(DTLZ1):
         super(DTLZ2, self).__init__(number_of_variables, number_of_objectives)
 
     def evaluate(self, solution: FloatSolution) -> FloatSolution:
-        k = self.number_of_variables - self.number_of_objectives + 1
+        k = self.number_of_variables() - self.number_of_objectives() + 1
 
-        g = sum([(x - 0.5) * (x - 0.5) for x in solution.variables[self.number_of_variables - k :]])
+        g = sum([(x - 0.5) * (x - 0.5) for x in solution.variables[self.number_of_variables() - k :]])
 
-        solution.objectives = [1.0 + g] * self.number_of_objectives
+        solution.objectives = [1.0 + g] * self.number_of_objectives()
 
-        for i in range(self.number_of_objectives):
-            for j in range(self.number_of_objectives - (i + 1)):
+        for i in range(self.number_of_objectives()):
+            for j in range(self.number_of_objectives() - (i + 1)):
                 solution.objectives[i] *= cos(solution.variables[j] * 0.5 * pi)
 
             if i != 0:
-                solution.objectives[i] *= sin(0.5 * pi * solution.variables[self.number_of_objectives - (i + 1)])
+                solution.objectives[i] *= sin(0.5 * pi * solution.variables[self.number_of_objectives() - (i + 1)])
 
         return solution
 
-    def get_name(self):
+    def name(self):
         return "DTLZ2"
 
 
@@ -120,7 +123,7 @@ class DTLZ3(DTLZ1):
 
         return solution
 
-    def get_name(self):
+    def name(self):
         return "DTLZ3"
 
 
@@ -153,7 +156,7 @@ class DTLZ4(DTLZ1):
 
         return solution
 
-    def get_name(self):
+    def name(self):
         return "DTLZ4"
 
 
@@ -191,7 +194,7 @@ class DTLZ5(DTLZ1):
 
         return solution
 
-    def get_name(self):
+    def name(self):
         return "DTLZ5"
 
 
@@ -229,7 +232,7 @@ class DTLZ6(DTLZ1):
 
         return solution
 
-    def get_name(self):
+    def name(self):
         return "DTLZ6"
 
 
@@ -259,5 +262,5 @@ class DTLZ7(DTLZ1):
 
         return solution
 
-    def get_name(self):
+    def name(self):
         return "DTLZ7"
