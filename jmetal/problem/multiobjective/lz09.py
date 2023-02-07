d61dc5bbeaddd98e5bcae40be1653cecb1218b39
ajnebro <ajnebro@users.noreply.github.com>
2023-02-07 16:27:03 +0100

    Refactor problems LZ09

--- jmetal/problem/multiobjective/lz09.py
+++ jmetal/problem/multiobjective/lz09.py
@@ -20,8 +20,6 @@ class LZ09(FloatProblem):
     def __init__(
         self,
         number_of_variables: int,
-        number_of_objectives: int,
-        number_of_constraints: int,
         ptype: int,
         dtype: int,
         ltype: int,
@@ -32,25 +30,22 @@ class LZ09(FloatProblem):
         IEEE Transactions on Evolutionary Computation, 12(2):284-302, April 2009.
         """
         super(LZ09, self).__init__()
-        self.number_of_variables = number_of_variables
-        self.number_of_objectives = number_of_objectives
-        self.number_of_constraints = number_of_constraints
 
-        self.obj_directions = [self.MINIMIZE, self.MINIMIZE, self.MINIMIZE]
-        self.obj_labels = ["f(x)", "f(y)", "f(z)"]
-
-        self.lower_bound = self.number_of_variables * [0.0]
-        self.upper_bound = self.number_of_variables * [1.0]
+        self.lower_bound = number_of_variables * [0.0]
+        self.upper_bound = number_of_variables * [1.0]
 
         self.ptype = ptype
         self.dtype = dtype
         self.ltype = ltype
 
+    def number_of_constraints(self) -> int:
+        return 0
+
     def evaluate(self, solution: FloatSolution) -> FloatSolution:
         x = solution.variables
         y = self.objective(x)
 
-        for i in range(self.number_of_objectives):
+        for i in range(self.number_of_objectives()):
             solution.objectives[i] = y[i]
 
         return solution
@@ -66,13 +61,13 @@ class LZ09(FloatProblem):
 
         if type == 21:
             xy = 2 * (x - 0.5)
-            beta = xy - math.pow(t1, 0.5 * (self.number_of_variables + 3 * dim - 8) / (self.number_of_variables - 2))
+            beta = xy - math.pow(t1, 0.5 * (self.number_of_variables() + 3 * dim - 8) / (self.number_of_variables() - 2))
         if type == 22:
-            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables
+            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables()
             xy = 2 * (x - 0.5)
             beta = xy - math.sin(theta)
         if type == 23:
-            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables
+            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables()
             ra = 0.8 * t1
             xy = 2 * (x - 0.5)
             if css == 1:
@@ -80,7 +75,7 @@ class LZ09(FloatProblem):
             else:
                 beta = xy - ra * math.sin(theta)
         if type == 24:
-            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables
+            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables()
             xy = 2 * (x - 0.5)
             ra = 0.8 * t1
             if css == 1:
@@ -90,7 +85,7 @@ class LZ09(FloatProblem):
         if type == 25:
             rho = 0.8
             phi = math.pi * t1
-            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables
+            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables()
             xy = 2 * (x - 0.5)
             if css == 1:
                 beta = xy - rho * math.sin(phi) * math.sin(theta)
@@ -99,7 +94,7 @@ class LZ09(FloatProblem):
             else:
                 beta = xy - rho * math.cos(phi)
         if type == 26:
-            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables
+            theta = 6 * math.pi * t1 + dim * math.pi / self.number_of_variables()
             ra = 0.3 * t1 * (t1 * math.cos(4 * theta) + 2)
             xy = 2 * (x - 0.5)
             if css == 1:
@@ -118,10 +113,10 @@ class LZ09(FloatProblem):
 
         if type == 31:
             xy = 4 * (x - 0.5)
-            rate = 1.0 * dim / self.number_of_variables
+            rate = 1.0 * dim / self.number_of_variables()
             beta = xy - 4 * (t1 * t1 * rate + t2 * (1.0 - rate)) + 2
         if type == 32:
-            theta = 2 * math.pi * t1 + dim * math.pi / self.number_of_variables
+            theta = 2 * math.pi * t1 + dim * math.pi / self.number_of_variables()
             xy = 4 * (x - 0.5)
             beta = xy - 2 * t2 * math.sin(theta)
 
@@ -200,11 +195,11 @@ class LZ09(FloatProblem):
         bb = []
         cc = []
 
-        y_objectives = [0.0] * self.number_of_objectives
+        y_objectives = [0.0] * self.number_of_objectives()
 
-        if self.number_of_objectives == 2:
+        if self.number_of_objectives() == 2:
             if self.ltype in [21, 22, 23, 24, 26]:
-                for n in range(1, self.number_of_variables):
+                for n in range(1, self.number_of_variables()):
                     if n % 2 == 0:
                         a = self.__ps_func2(x_variables[n], x_variables[0], n, self.ltype, 1)
                         aa.append(a)
@@ -220,7 +215,7 @@ class LZ09(FloatProblem):
                 y_objectives[0] = alpha[0] + h
                 y_objectives[1] = alpha[1] + g
             if self.ltype == 25:
-                for n in range(1, self.number_of_variables):
+                for n in range(1, self.number_of_variables()):
                     if n % 3 == 0:
                         a = self.__ps_func2(x_variables[n], x_variables[0], n, self.ltype, 1)
                         aa.append(a)
@@ -242,10 +237,10 @@ class LZ09(FloatProblem):
                 y_objectives[0] = alpha[0] + h
                 y_objectives[1] = alpha[1] + g
 
-        if self.number_of_objectives == 3:
+        if self.number_of_objectives() == 3:
             if self.ltype == 31 or self.ltype == 32:
 
-                for n in range(2, self.number_of_variables):
+                for n in range(2, self.number_of_variables()):
                     a = self.__ps_func3(x_variables[n], x_variables[0], x_variables[1], n, self.ltype)
 
                     if n % 3 == 0:
@@ -272,90 +267,138 @@ class LZ09(FloatProblem):
 
 
 class LZ09_F1(LZ09):
-    def __init__(self):
+    def __init__(self, number_of_variables=10):
         super(LZ09_F1, self).__init__(
-            number_of_variables=10, number_of_objectives=2, number_of_constraints=0, dtype=1, ltype=21, ptype=21
+            number_of_variables, dtype=1, ltype=21, ptype=21
         )
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ["f(x)", "f(y)"]
 
-    def get_name(self):
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def name(self):
         return "LZ09_F1"
 
 
 class LZ09_F2(LZ09):
-    def __init__(self):
+    def __init__(self, number_of_variables=30):
         super(LZ09_F2, self).__init__(
-            number_of_variables=30, number_of_objectives=2, number_of_constraints=0, dtype=1, ltype=22, ptype=21
+            number_of_variables, dtype=1, ltype=22, ptype=21
         )
 
-    def get_name(self):
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ["f(x)", "f(y)"]
+
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def name(self):
         return "LZ09_F2"
 
 
 class LZ09_F3(LZ09):
-    def __init__(self):
+    def __init__(self, number_of_variables=30):
         super(LZ09_F3, self).__init__(
-            number_of_variables=30, number_of_objectives=2, number_of_constraints=0, dtype=1, ltype=23, ptype=21
+            number_of_variables, dtype=1, ltype=23, ptype=21
         )
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ["f(x)", "f(y)"]
 
-    def get_name(self):
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def name(self):
         return "LZ09_F3"
 
 
 class LZ09_F4(LZ09):
-    def __init__(self):
+    def __init__(self, number_of_variables=30):
         super(LZ09_F4, self).__init__(
-            number_of_variables=30, number_of_objectives=2, number_of_constraints=0, dtype=1, ltype=24, ptype=21
+            number_of_variables, dtype=1, ltype=24, ptype=21
         )
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ["f(x)", "f(y)"]
 
-    def get_name(self):
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def name(self):
         return "LZ09_F4"
 
 
 class LZ09_F5(LZ09):
-    def __init__(self):
+    def __init__(self, number_of_variables=30):
         super(LZ09_F5, self).__init__(
-            number_of_variables=30, number_of_objectives=2, number_of_constraints=0, dtype=1, ltype=26, ptype=21
+            number_of_variables, dtype=1, ltype=26, ptype=21
         )
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ["f(x)", "f(y)"]
 
-    def get_name(self):
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def name(self):
         return "LZ09_F5"
 
 
 class LZ09_F6(LZ09):
-    def __init__(self):
+    def __init__(self, number_of_variables=10):
         super(LZ09_F6, self).__init__(
-            number_of_variables=10, number_of_objectives=3, number_of_constraints=0, dtype=1, ltype=32, ptype=31
+            number_of_variables, dtype=1, ltype=32, ptype=31
         )
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ["f(x)", "f(y)", "f(z)"]
 
-    def get_name(self):
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def name(self):
         return "LZ09_F6"
 
 
 class LZ09_F7(LZ09):
-    def __init__(self):
+    def __init__(self, number_of_variables=10):
         super(LZ09_F7, self).__init__(
-            number_of_variables=10, number_of_objectives=2, number_of_constraints=0, dtype=3, ltype=21, ptype=21
+            number_of_variables, dtype=3, ltype=21, ptype=21
         )
 
-    def get_name(self):
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ["f(x)", "f(y)"]
+
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def name(self):
         return "LZ09_F7"
 
 
 class LZ09_F8(LZ09):
-    def __init__(self):
+    def __init__(self, number_of_variables=10):
         super(LZ09_F8, self).__init__(
-            number_of_variables=10, number_of_objectives=2, number_of_constraints=0, dtype=4, ltype=21, ptype=21
+            number_of_variables, dtype=4, ltype=21, ptype=21
         )
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ["f(x)", "f(y)"]
 
-    def get_name(self):
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def name(self):
         return "LZ09_F8"
 
 
 class LZ09_F9(LZ09):
-    def __init__(self):
+    def __init__(self, number_of_variables=30):
         super(LZ09_F9, self).__init__(
-            number_of_variables=30, number_of_objectives=2, number_of_constraints=0, dtype=1, ltype=22, ptype=22
+            number_of_variables, dtype=1, ltype=22, ptype=22
         )
 
-    def get_name(self):
+        self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
+        self.obj_labels = ["f(x)", "f(y)"]
+
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def name(self):
         return "LZ09_F9"
