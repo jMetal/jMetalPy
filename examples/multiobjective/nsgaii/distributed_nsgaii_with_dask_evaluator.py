d0dcaaa5320239382a17affaf46dacbc0f764812
ajnebro <ajnebro@users.noreply.github.com>
2024-11-25 09:01:03 +0100

    Fix a bug in the example of distributed NSGA-II with dask evaluator

--- examples/multiobjective/nsgaii/distributed_nsgaii_with_dask_evaluator.py
+++ examples/multiobjective/nsgaii/distributed_nsgaii_with_dask_evaluator.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         problem=problem,
         population_size=10,
         offspring_population_size=10,
-        mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=20),
+        mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables(), distribution_index=20),
         crossover=SBXCrossover(probability=1.0, distribution_index=20),
         population_evaluator=DaskEvaluator(),
         termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations),
