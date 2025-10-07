84daef5e832d95120f32bebb29672ec8d498879d
ajnebro <ajnebro@users.noreply.github.com>
2025-10-07 12:35:08 +0200

    Minor changes

--- src/jmetal/__init__.py
+++ src/jmetal/__init__.py
@@ -1,7 +1,7 @@
-from jmetal import algorithm, core, operator, problem, metaopt
+from jmetal import algorithm, core, operator, problem
 from jmetal.logger import configure_logging
 
 configure_logging()
 
-__all__ = ["core", "algorithm", "operator", "problem", "metaopt"]
+__all__ = ["core", "algorithm", "operator", "problem"]
 __version__ = "1.7.1"
--- src/jmetal/metaopt/__init__.py
+++
@@ -1,3 +0,0 @@
-from jmetal.metaopt import component
-
-__all__ = ["component"]
