23a185c9630acc10f76ef8b8499b103d64b5e7ef
ajnebro <ajnebro@users.noreply.github.com>
2025-11-18 17:16:26 +0100

    Add example of NSGA-III with a 4D problem

---
+++ examples/multiobjective/nsgaiii/nsgaiii_dtlz2_4D.py
@@ -0,0 +1,39 @@
+from jmetal.algorithm.multiobjective.nsgaiii import (
+    NSGAIII,
+    UniformReferenceDirectionFactory,
+)
+from jmetal.operator.crossover import SBXCrossover
+from jmetal.operator.mutation import PolynomialMutation
+from jmetal.problem import DTLZ2
+from jmetal.util.solution import (
+    get_non_dominated_solutions,
+    print_function_values_to_file,
+    print_variables_to_file,
+    read_solutions,
+)
+from jmetal.util.termination_criterion import StoppingByEvaluations
+
+if __name__ == "__main__":
+    problem = DTLZ2(12, 4)
+
+    max_evaluations = 30000
+
+    algorithm = NSGAIII(
+        problem=problem,
+        population_size=100,
+        reference_directions=UniformReferenceDirectionFactory(4, n_points=100),
+        mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables(), distribution_index=20),
+        crossover=SBXCrossover(probability=1.0, distribution_index=20),
+        termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations),
+    )
+
+    algorithm.run()
+    front = get_non_dominated_solutions(algorithm.result())
+
+    # Save results to file
+    print_function_values_to_file(front, "FUN." + algorithm.label)
+    print_variables_to_file(front, "VAR." + algorithm.label)
+
+    print(f"Algorithm: {algorithm.get_name()}")
+    print(f"Problem: {problem.name()}")
+    print(f"Computing time: {algorithm.total_computing_time}")
